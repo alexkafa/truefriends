979af9cfda0ff6c222688011da2e0127c15cb5d0
Αλέξ <alexkafa@csd.auth.gr>
2024-05-16 20:02:40 +0300

    16/5 pre app running

---
+++ app/src/main/java/com/example/truefriends/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.truefriends;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    EditText objEditTextName;
+    TextView objTextViewName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void openTeamSetUpPage(View view) {
+        Intent intent = new Intent(this, second.class);
+        startActivity(intent);
+    }
+}
