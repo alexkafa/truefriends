86d89f1d96fdfa0c94802e4a183f2f1c95e7c276
Αλέξ <alexkafa@csd.auth.gr>
2024-05-19 22:18:31 +0300

    back/next buttons usability create the actionview of categories

--- app/src/main/java/com/example/truefriends/second.java
+++ app/src/main/java/com/example/truefriends/second.java
@@ -1,6 +1,10 @@
 package com.example.truefriends;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class second extends AppCompatActivity {
@@ -8,5 +12,21 @@ public class second extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        findViewById(R.id.buttonNext).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(second.this, third.class);
+                startActivity(intent);
+            }
+        });
+        Button buttonBack = findViewById(R.id.buttonBack);
+        buttonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish(); // Closes the current activity and returns to the previous one
+            }
+        });
     }
+
 }
---
+++ app/src/main/java/com/example/truefriends/third.java
@@ -0,0 +1,13 @@
+// ThirdActivity.java
+package com.example.truefriends;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class third extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+    }
+}
