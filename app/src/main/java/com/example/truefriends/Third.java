07ef766ccc7ab3158934ee8146aa1afa8cd39c81
AthinaSw <swingathina@gmail.com>
2024-05-26 20:22:29 +0300

    Saving chosen category

--- app/src/main/java/com/example/truefriends/Third.java
+++ app/src/main/java/com/example/truefriends/Third.java
@@ -18,40 +18,40 @@ public class Third extends AppCompatActivity {
     public void showQuestion(View view) {
         Intent intent = new Intent(this, QuestionActivity.class);
         String category = "Default category";
+        int id = view.getId();
 
-        switch (view.getId()) {
-            case 1:
-                category = "Dates";
-                break;
-            case 2:
-                category = "Hopes and Dreams";
-                break;
-            case 3:
-                category = "Gossip";
-                break;
-            case 4:
-                category = "Binary";
-                break;
-            case 5:
-                category = "Top 5";
-                break;
-            case 6:
-                category = "Names";
-                break;
-            case 7:
-                category = "Ancient History";
-                break;
-            case 8:
-                category = "Hobbies";
-                break;
-            case 9:
-                category = "True Friends";
-                break;
+        if (id == R.id.imageButton1) {
+            category = "Dates";
         }
-        System.out.println(category);
+        else if (id == R.id.imageButton2) {
+            category = "Hopes and Dreams";
+        }
+        else if (id == R.id.imageButton3) {
+            category = "Gossip";
+        }
+        else if (id == R.id.imageButton4) {
+            category = "Binary";
+        }
+        else if (id == R.id.imageButton5) {
+            category = "Top 5";
+        }
+        else if (id == R.id.imageButton6) {
+            category = "Names";
+        }
+        else if (id == R.id.imageButton7) {
+            category = "Ancient History";
+        }
+        else if (id == R.id.imageButton8) {
+            category = "Hobbies";
+        }
+        else {
+            category = "True Friends";
+        }
+
+        //System.out.println(category);
 
         intent.putExtra("QUESTION", category);
-       // startActivity(intent);
+        startActivity(intent);
     }
 
     public void cancel(View view) {
