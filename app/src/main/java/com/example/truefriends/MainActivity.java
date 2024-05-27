6599a6b7ed3bb8602c59528b87e14d72d8685d96
AthinaSw <swingathina@gmail.com>
2024-05-27 18:39:54 +0300

    gui

--- app/src/main/java/com/example/truefriends/MainActivity.java
+++ app/src/main/java/com/example/truefriends/MainActivity.java
@@ -3,8 +3,6 @@ package com.example.truefriends;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
--- app/src/main/java/com/example/truefriends/RulesActivity.java
+++ app/src/main/java/com/example/truefriends/RulesActivity.java
@@ -2,6 +2,7 @@ package com.example.truefriends;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,6 +11,23 @@ public class RulesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rules);
+
+        String rules = "Have you seen this episode on Friends, where the girls argue with the " +
+                "boys about who knows whom the best and then they end up making a quiz game to actually " +
+                "find out? Well, with this game it's your turn to find out how well you and your friends " +
+                "know each other!\nThe rules are simple; Divide your friend group in two teams and pick " +
+                "names for your teams. After that, you go through 20 rounds of questions that can be either " +
+                "answered right or wrong - your opponent team decides that. Before each question, you get " +
+                "to pick the category (see categories bellow). In the end the team with the highest score wins" +
+                " and gets the good apartment...erh sorry...you can choose your own bet!\n\n\n" +
+                "Categories:\n1. Dates: Questions that are answered with a date.\n2. Hopes and Dreams: " +
+                "Questions about your friends' future plans\n3. Gossip: No explanation needed.\n4. Binary: Yes/No " +
+                "questions.\n5. Top 5: Name the top 5...\n6. Names: Questions that are answered with names.\n" +
+                "7. Ancient History: A.k.a. past events.\n8. Hobbies: A.k.a hobbies\n9. True Friends: " +
+                "Your own 2 questions!";
+
+        TextView textRules = findViewById(R.id.textView2);
+        textRules.setText(rules);
     }
     public void goBackToMain(View view) {
         finish(); // Κλείνει την τρέχουσα δραστηριότητα και επιστρέφει στην προηγούμενη
